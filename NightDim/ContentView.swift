c922022ae5fd3a6bf5a97c5451841b6e36c13958
ulasyesilyurt <ulasyesilyurt.alagoz24@gmail.com>
2026-09-05 02:06:21 +0300

    Initial Commit

---
+++ NightDim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NightDim
+//
+//  Created by Ulaş on 5.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NightDim/NightDimApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightDimApp.swift
+//  NightDim
+//
+//  Created by Ulaş on 5.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct NightDimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
